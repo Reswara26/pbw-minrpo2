aba31a88d1ff989fe66ecc973a3a4bbca01764c1
Reswara26 <ganendra.rashi96@gmail.com>
2026-03-31 21:57:19 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,23 +1,12 @@
--- =============================================
---  DATABASE: portfolio_reswara
---  Cara pakai:
---  1. Buka phpMyAdmin di browser (http://localhost/phpmyadmin)
---  2. Klik tab "SQL"
---  3. Paste seluruh isi file ini
---  4. Klik tombol "Go"
--- =============================================
-
 CREATE DATABASE IF NOT EXISTS portfolio_reswara;
 USE portfolio_reswara;
 
--- Tabel skills
 CREATE TABLE IF NOT EXISTS skills (
   id      INT AUTO_INCREMENT PRIMARY KEY,
   nama    VARCHAR(100) NOT NULL,
   persen  INT          NOT NULL
 );
 
--- Isi data skills
 INSERT INTO skills (nama, persen) VALUES
   ('MySQL & Database', 80),
   ('HTML & CSS',       75),
